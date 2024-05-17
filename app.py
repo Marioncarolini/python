3c0bb7b51e9fce73ffa0a6f709499221f5583619
Marioncarolini <166819516+Marioncarolini@users.noreply.github.com>
2024-05-17 08:51:13 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,3 @@
+dias_da_semana = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']
+dias_da_semana_ordenados = sorted(dias_da_semana)
+print(dias_da_semana_ordenados)
